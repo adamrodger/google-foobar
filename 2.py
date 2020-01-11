d8f3274f2d43fa9575fdee035a7c9c7a86e244cb
Adam Rodger <adam.rodger@gmail.com>
2020-01-11 23:18:51 +0000

    Challenge 2 - much better single pass solution

--- 2.py
+++ 2.py
@@ -1,55 +1,23 @@
-# approach: 
-# build a tree using recursion - keep splitting elements until you've only got one left
-# traverse the tree to find targets, then unwind one, collecting results
-
-class Node:
-    def __init__(self, data):
-        self.data = data
-        self.left = self.right = None
+def build_lookup(numbers, parent, lookup = {}):
+    # populate a lookup of node:parent by traversing the tree once
+    node = numbers[-1]
+    lookup[node] = parent
     
-    def __str__(self):
-        return "%d: [left: %s, right: %s]" % (self.data, self.left, self.right)
-        
-
-def build_tree(numbers):
-    node = Node(numbers[-1])
     radix = int((len(numbers) - 1) / 2)
     
-    if radix == 0:
-        # no more child nodes
-        return node
+    if radix > 0:
+        build_lookup(numbers[0:radix], node, lookup)
+        build_lookup(numbers[radix:-1], node, lookup)
     
-    node.left = build_tree(numbers[0:radix])
-    node.right = build_tree(numbers[radix:-1])
-    return node
-
-def find_parent(node, parent, target):
-    if node == None:
-        return -1
-        
-    if node.data == target:
-        return -1 if parent == None else parent.data
-    
-    found_left = find_parent(node.left, node, target)
-    if found_left > 0:
-        return found_left
-    
-    found_right = find_parent(node.right, node, target)
-    if found_right > 0:
-        return found_right
-        
-    # not found - magic number
-    return -1
+    return lookup
 
 def solution(h, q):
     numbers = list(range(1, 2**h))
-    root = build_tree(numbers)
+    lookup = build_lookup(numbers, -1)
     results = []
     
-    # bit naive because we'll recurse the tree for each target instead
-    # of collecting as we recurse
     for target in q:
-        parent = find_parent(root, None, target)
+        parent = lookup[target] if target in lookup else -1
         results.append(parent)
         
     return results
