c0e5bf78c53f6048fbbe19f2d11d1db29bdef28f
Adam Rodger <adam.rodger@gmail.com>
2020-01-14 20:22:36 +0000

    Level 4 part 1 - small tidy up

--- 4-1.py
+++ 4-1.py
@@ -6,20 +6,23 @@ def solution(num_buns, num_required):
     - you have n bunnies
     - you require exactly r bunnies in order to open the doors (i.e. there are that many consoles)
         - r choose k is combinatorics - learned that in Project Euler :)
-    - so, each key must have n-r+1 copies - e.g. n=2 and r=2 == 1 copy of each key, which makes sense becaue it's [[0], [1]]
+    - so, k = n-r+1 - e.g. n=2 and r=2 == 1 copy of each key, which makes sense becaue it's [[0], [1]]
         - the example with n=5, r=3 makes that clearer - each key appears exactly 3 times (5-3+1)
-    - so, for each combination of r bunnies, make sure that combination has a key that no other combination has
+        - 5 choose 3 == 10 so the keys are 0-9
+    - so, for each combination of k bunnies, make sure that combination has a key that no other combination has
     """
     bunnies = [[] for bunny in xrange(num_buns)]
 
-    # divide the bunnies into group combinations - i.e. each bunny will appear in multiple groups
-    groups = combinations(bunnies, num_buns - num_required + 1)
+    # divide the bunnies into combinations of k size - i.e. each bunny will appear in multiple groups
+    k = num_buns - num_required + 1
+    groups = combinations(bunnies, k)
 
+    # everyone in a group gets a key matching the group ID
     for key, group in enumerate(groups):
         for bunny in group:
-            # everyone in that group gets the key
             bunny.append(key)
 
+    # fortunately this is already formatted properly for the required output
     return bunnies
 
 if __name__ == "__main__":
